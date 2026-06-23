6581ab79bad4e6a644915deac0de68418385a813
gaku0071-bit <gaku0071@gmail.com>
2026-06-24 01:19:22 +0900

    Cache Realpro parsing patch

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "rent-estimate-v24";
-const APP_SHELL = ["/", "/index.html", "/styles.css?v=20260624-2", "/app.js?v=20260624-2", "/manifest.json", "/icon.svg", "/store-logo.png"];
+const CACHE_NAME = "rent-estimate-v25";
+const APP_SHELL = ["/", "/index.html", "/styles.css?v=20260624-2", "/app.js?v=20260624-2", "/app-patch.js?v=20260624-3", "/manifest.json", "/icon.svg", "/store-logo.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
