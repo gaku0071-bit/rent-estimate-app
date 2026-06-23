36ee2c1b5566d04df11cef3f26591d4a713eea17
gaku0071-bit <gaku0071@gmail.com>
2026-06-24 01:14:14 +0900

    Update service worker cache for Realpro parsing fixes

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "rent-estimate-v23";
-const APP_SHELL = ["/", "/index.html", "/styles.css?v=20260624-1", "/app.js?v=20260624-1", "/manifest.json", "/icon.svg", "/store-logo.png"];
+const CACHE_NAME = "rent-estimate-v24";
+const APP_SHELL = ["/", "/index.html", "/styles.css?v=20260624-2", "/app.js?v=20260624-2", "/manifest.json", "/icon.svg", "/store-logo.png"];
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
